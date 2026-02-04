b387c6de72edd02e19b77b181d7e03038f9642f8
Travis Miller <radezorack@gmail.com>
2026-02-04 18:52:59 -0500

    Added Elysia
    A http server built for Bun

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .get("/", () => "Hello from Elysia + Bun!")
+  .get("/health", () => ({ ok: true }))
+  .listen(3000);
+
+console.log(
+  `Elysia server running at http://${app.server?.hostname}:${app.server?.port}`
+);
